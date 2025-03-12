6b17364ab8968e153453481a0d16aa1616d10679
nixonantony <nixonantonyrajan@gmail.com>
2025-03-12 20:37:53 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,54 @@
+console.log("Hello World");
+
+let A = 45;
+let B = 15;
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(A / B);
+
+for(let i = 1; i <= 10; i++)
+{
+	console.log(i);
+}
+
+let j = 1;
+while(j < 11)
+{
+	console.log(j);
+	j++;
+}
+
+for(let k = 10; k > 1; k--)
+{
+	console.log(k);
+}
+
+for(let m = 0; m <= 10; m++)
+{
+	if(m % 2 == 1)
+	{
+		console.log(m);
+	}
+}
+
+let r = 15;
+let s;
+let t = parseInt((r / 2) + 1);
+
+if(r % 2 == 1)
+{
+	for(s = t - 1; s > 0; s--)
+	{
+		console.log(i);
+	}
+	
+	console.log("-" + m + "-");
+	
+	for(s = r; s >= t + 1; s--)
+	{
+		console.log(i);
+	}
+
+}
